efcb35636dacf736bb08a9bc65b5456244992db7
Oluwatobi <oluwatobioreniyi@gmail.com>
2025-08-09 04:33:58 +0100

    feat: enhance Firebase messaging service worker and app integration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -12,8 +12,34 @@ export const firebaseConfig = {
 
 export const FIREBASE_VAPID_KEY = import.meta.env.VITE_VAPID_KEY;
 
+console.log("Firebase client config:", firebaseConfig);
+console.log("Firebase VAPID Key:", FIREBASE_VAPID_KEY);
+
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
+
+navigator.serviceWorker
+  .register("/firebase-messaging-sw.js")
+  .then((registration) => {
+    return getToken(messaging, {
+      vapidKey: import.meta.env.VITE_VAPID_KEY,
+      serviceWorkerRegistration: registration,
+    });
+  })
+  .then((currentToken) => {
+    if (currentToken) {
+      console.log("FCM token:", currentToken);
+      // TODO: send this token to your backend for sending notifications
+      return currentToken;
+    } else {
+      console.log(
+        "No registration token available. Request permission to generate one."
+      );
+    }
+  })
+  .catch((err) => {
+    console.error("An error occurred while retrieving token. ", err);
+  });
 
 export const requestForToken = () => {
   return getToken(messaging, { vapidKey: FIREBASE_VAPID_KEY })
